c1aee716b35ca485cbba9f6d5285d8b5a5b511a3
Ayush Pattanayak <ayushmp7@gmail.com>
2022-02-01 13:04:10 +0530

    Updated Projects & footer sec

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,34 @@
-import {FiTerminal} from "react-icons/fi";
+import React from 'react';
+import { FaInstagram, FaTwitter, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { HiMail } from 'react-icons/hi';
 
-function Footer() {
+
+export default function Footer() {
   return (
-    <footer className="flex h-24 w-full items-center justify-center border-t">
-    <p className="flex items-center justify-center font-semibold text-gray-500 hover:text-gray-700 space-x-2">
-      Developed by {'  '}
-      <FiTerminal />
-      <a className="">
-        Ayush Pattanyak.
-      </a>
-    </p>
-  </footer>
-  );
+  
+<footer className="bg-white w-full py-4">
+    <div className="max-w-screen-xl mx-auto px-4">
+        <div className="pt-4 flex max-w-xs mx-auto items-center justify-between">
+            <a href="#" class="text-gray-500 hover:text-gray-800 dark:hover:text-white transition-colors duration-200">
+              <FaInstagram className='text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200'/>
+            </a>
+            <a href="#" class="text-gray-500 hover:text-gray-800 dark:hover:text-white transition-colors duration-200">
+              <FaLinkedinIn className='text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200'/>
+            </a>
+            <a href="#" class="text-gray-500 hover:text-gray-800 dark:hover:text-white transition-colors duration-200">
+              <FaGithub className='text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200'/>
+            </a>
+            <a href="#" class="text-gray-500 hover:text-gray-800 dark:hover:text-white transition-colors duration-200">
+              <FaTwitter className='text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200'/>
+            </a>
+            <a href="#" class="text-gray-500 hover:text-gray-800 dark:hover:text-white transition-colors duration-200">
+              <HiMail className='text-xl hover:text-gray-800 dark:hover:text-white transition-colors duration-200'/>
+            </a>
+        </div>
+        <a class="text-center text-gray-400 hover:text-gray-600 pt-8 sm:pt-10 flex items-center justify-center font-semibold transition-colors duration-200">
+          Developed by Ayush Pattanayak
+        </a>
+    </div>
+</footer>
+)
 }
-
-export default Footer;
